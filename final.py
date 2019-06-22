c6d80861eff26a17e3e94d8f2564faea1d5c7a29
Chirag Sharma <chiragsharmaindian@gmail.com>
2019-06-22 14:19:50 +0530

    Initail commit

---
+++ final.py
@@ -0,0 +1,58 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = cv.imread("images/qproj1_crop.png")
+img2 = cv.imread("images/qproj2_crop.png")
+img3 = cv.imread("images/qproj3_crop.png")
+
+
+
+# b,g,r = cv.split(img)
+# img = cv.merge([r,g,b])
+gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+gray3 = cv.cvtColor(img3, cv.COLOR_BGR2GRAY)
+ret1, threshold1 = cv.threshold(gray1,160,255,cv.THRESH_BINARY)
+ret2, threshold2 = cv.threshold(gray2,160,255,cv.THRESH_BINARY)
+ret3, threshold3 = cv.threshold(gray3,160,255,cv.THRESH_BINARY)
+
+
+# gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+nonzero1 = cv.countNonZero(threshold1)
+total1 = img1.shape[0] * img1.shape[1]
+zero1 = total1 - nonzero1
+ratio1 = nonzero1 * 100 / float(total1)
+
+
+nonzero2 = cv.countNonZero(threshold2)
+total2 = img2.shape[0] * img2.shape[1]
+zero2 = total2 - nonzero2
+ratio2 = nonzero2 * 100 / float(total2)
+
+
+nonzero3 = cv.countNonZero(threshold3)
+total3 = img3.shape[0] * img3.shape[1]
+zero3 = total3 - nonzero3
+ratio3 = nonzero3 * 100 / float(total3)
+
+# print("The number of black pixels are: {}".format(nonzero))
+# # print(zero)
+# print(total)
+# print(zero)
+print("Heart line values: {}".format(ratio1))
+print("Head line values: {}".format(ratio2))
+print("Life Line: {}".format(ratio3))
+
+# age_parameter = 140*ratio/100
+
+# print("The age parameter is {}".format(age_parameter))
+
+#These value are true & are based on ideal conditions means...if you eat healthy + physically fit + mostly panic free
+
+
+# plt.imshow(gray_image,'gray')
+# # plt.imshow(gray)
+# plt.xticks(), plt.yticks()
+# plt.show()
---
+++ proj1_crop.py
@@ -0,0 +1,28 @@
+from PIL import Image
+def main():
+	try:
+		#Relative path
+		#Image of right hand
+		img = Image.open("images/hh1.png") 
+		width, height = img.size
+		area = (0,220,int(width),int(height/1.16))
+		area1 = (0,240,int(width),int(height/1.65))
+		area2 = (0,280,int(width),int(height/1.55))
+		area3 = (int(width/2),280,int(width),int(height/1.3))
+		# img_original = img
+		img_default = img.crop(area)
+		img1 = img.crop(area1)
+		img2 = img.crop(area2)
+		img3 = img.crop(area3)
+		# Saved in the same relative location
+		img_default.save("images/qproj_crop.png")
+		img1.save("images/qproj1_crop.png")	# Heart line - heart_parameter
+		img2.save("images/qproj2_crop.png")	# Head line---head_parameter
+		img3.save("images/qproj3_crop.png") # life line---age_parameter
+		# Heart Line - shows a person's attitude to love and quality of love.
+		# Head Line - reveals a person's wisdom, belief, attitude, thinking ability, strain capacity, creative ability as well as abilities of memory, self-control and more. 
+		# LIfe line - age & mainly reflects a person's physical vitality and life energy
+	except IOError:
+		pass
+if __name__ == "__main__":
+	main()
